71e73d7e5070c7ca6499bd99f8f51259fc9bb1bc
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-10-21 18:23:37 +0200

    Start main controller class

---
+++ redsun/controller/controller.py
@@ -0,0 +1,10 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from redsun.toolkit.config import RedSunInstanceInfo
+
+class RedSunHardwareController:
+    """ Main hardware controller.
+    """
+    def __init__(self, 
+                 instance_info: "RedSunInstanceInfo") -> None:
+        pass
