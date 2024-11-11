e0cf62bc5008592f5debe96fc53647c9a798ab72
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-11 18:13:05 +0100

    Add first test

---
+++ src/redsun/tests/test_factory.py
@@ -0,0 +1,9 @@
+from redsun.factory import get_available_engines
+from redsun.engine.exengine import ExEngineHandler
+
+def test_handlers_dict() -> None:
+    """Test the content of the handlers dictionary."""
+    engines = get_available_engines()
+    assert len(engines) > 0
+    assert "exengine" in engines
+    assert engines["exengine"] == ExEngineHandler
