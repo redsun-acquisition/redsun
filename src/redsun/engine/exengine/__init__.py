d70fecd95f193589d09c9d0d1d3f395b7ca1cc3f
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-22 21:25:27 +0100

    try-except for missing import

--- src/redsun/engine/exengine/__init__.py
+++ src/redsun/engine/exengine/__init__.py
@@ -1,5 +1,10 @@
 # noqa: D100, D104
+try:
+    from .handler import ExEngineHandler
+except ImportError:
 
-from .handler import ExEngineHandler
+    class ExEngineHandler:  # type: ignore[no-redef] # noqa: D101
+        ...
+
 
 __all__ = ["ExEngineHandler"]
