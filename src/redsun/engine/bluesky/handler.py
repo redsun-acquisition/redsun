88ff1101b2b72c477ce2c2ce50b47eea7c97d3a5
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-11-25 13:34:36 +0100

    Add more bluesky implementation

--- src/redsun/engine/bluesky/handler.py
+++ src/redsun/engine/bluesky/handler.py
@@ -2,12 +2,12 @@
 
 from typing import TYPE_CHECKING
 
-from bluesky import RunEngine
-from sunflare.engine import DetectorModel, EngineHandler, MotorModel
+from bluesky.run_engine import RunEngine
+from sunflare.engine import EngineHandler, DetectorModel, MotorModel
 from sunflare.errors import UnsupportedDeviceType
 
 if TYPE_CHECKING:
-    from typing import Any, Dict
+    from typing import Any, Union
 
     from sunflare.config import RedSunInstanceInfo
     from sunflare.engine.bluesky import BlueskyDetectorModel, BlueskyMotorModel
@@ -30,6 +30,9 @@ class BlueskyHandler(EngineHandler):
         The virtual bus instance for the module.
     """
 
+    _detectors: "dict[str, BlueskyDetectorModel]" = {}
+    _motors: "dict[str, BlueskyMotorModel]" = {}
+
     def __init__(
         self,
         config_options: "RedSunInstanceInfo",
@@ -37,3 +40,41 @@ class BlueskyHandler(EngineHandler):
         module_bus: "VirtualBus",
     ) -> None:
         super().__init__(config_options, virtual_bus, module_bus)
+        self._engine = RunEngine()  # type: ignore[no-untyped-call]
+
+    def register_device(  # noqa: D102
+        self, name: str, device: "Union[BlueskyDetectorModel, BlueskyMotorModel]"
+    ) -> None:
+        if isinstance(device, DetectorModel):
+            self._detectors[name] = device
+        elif isinstance(device, MotorModel):
+            self._motors[name] = device
+        else:
+            raise ValueError(
+                f"Device of type {type(device)} not supported by ExEngine."
+            )
+
+    @property
+    def detectors(self) -> "dict[str, BlueskyDetectorModel]":
+        """Dictionary containing all the registered ExEngine detectors."""
+        return self._detectors
+
+    @property
+    def motors(self) -> "dict[str, BlueskyMotorModel]":
+        """Dictionary containing all the registered ExEngine motors."""
+        return self._motors
+
+    @property
+    def engine(self) -> RunEngine:
+        """Execution engine instance."""
+        return self._engine
+
+    @property
+    def lights(self) -> "dict[str, Any]":
+        """Dictionary containing all the registered ExEngine light sources."""
+        raise UnsupportedDeviceType("Bluesky", "Light")
+
+    @property
+    def scanners(self) -> "dict[str, Any]":
+        """Dictionary containing all the registered ExEngine scanners."""
+        raise UnsupportedDeviceType("Bluesky", "Scanner")
