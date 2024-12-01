5b600127fb0d0242a385d0984d60b66260be2fdc
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 15:18:08 +0100

    Add import protection

--- src/redsun/engine/bluesky/__init__.py
+++ src/redsun/engine/bluesky/__init__.py
@@ -1,5 +1,10 @@
-# noqa: D104
+# noqa: D100, D104
+try:
+    from .handler import BlueskyHandler
+except ImportError:
 
-from .handler import BlueskyHandler
+    class BlueskyHandler:  # type: ignore[no-redef] # noqa: D101
+        ...
+
 
 __all__ = ["BlueskyHandler"]
