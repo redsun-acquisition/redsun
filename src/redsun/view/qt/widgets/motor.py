6be3df93fcaf5ca8a14dba4d03e6fd014f868b17
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 20:23:28 +0100

    Add more implementation

--- src/redsun/view/qt/widgets/motor.py
+++ src/redsun/view/qt/widgets/motor.py
@@ -6,19 +6,32 @@ from typing import TYPE_CHECKING
 
 from sunflare.view.qt import BaseWidget
 from sunflare.virtualbus import Signal
+from sunflare.log import Loggable
 
 from qtpy import QtWidgets
-from qtpy.QtCore import Qt
+from qtpy.QtGui import QRegularExpressionValidator
+from qtpy.QtCore import Qt, QRegularExpression
 
 if TYPE_CHECKING:
+    from typing import Any, Tuple
+
     from redsun.controller.virtualbus import HardwareVirtualBus
 
     from sunflare.config import MotorModelInfo
     from sunflare.virtualbus import VirtualBus
 
 
-class StepperMotorWidget(BaseWidget):
-    """Qt motor widget class.
+class StepperMotorWidget(BaseWidget, Loggable):
+    """Qt stepper motor widget.
+
+    The widget groups each motor into a QGroupBox, and each axis of the motor has its own row.
+
+    The widget gives users the ability to control the following options for each axis:
+    - moving the motor up and down;
+    - changing the step size.
+
+    Whenever a change happens in the GUI, the virtual bus is notified, and from there
+    the signals is routed to the motor controller.
 
     Parameters
     ----------
@@ -35,6 +48,7 @@ class StepperMotorWidget(BaseWidget):
     # but first the virtual bus needs to be reworked
     sigStepUp: Signal = Signal(str, str)
     sigStepDown: Signal = Signal(str, str)
+    sigStepSizeChanged: Signal = Signal(str, str, float)
 
     def __init__(
         self,
@@ -46,38 +60,101 @@ class StepperMotorWidget(BaseWidget):
 
         self.grid = QtWidgets.QGridLayout()
         self.setLayout(self.grid)
+        self.groups: dict[str, QtWidgets.QGroupBox] = {}
+        self.labels: dict[str, QtWidgets.QLabel] = {}
+        self.buttons: dict[str, QtWidgets.QPushButton] = {}
+        self.lineEdits: dict[str, QtWidgets.QLineEdit] = {}
 
-        for name, info in motors_info.items():
-            group = QtWidgets.QGroupBox(name)
-            group.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-            labels: dict[str, QtWidgets.QLabel] = {}
-            buttons: dict[str, QtWidgets.QPushButton] = {}
-            lineEdits: dict[str, QtWidgets.QLineEdit] = {}
+        # Regular expression for a valid floating-point number
+        float_regex = QRegularExpression(r"^[-+]?\d*\.?\d+$")
+        self.validator = QRegularExpressionValidator(float_regex)
+
+        for num_motor, (name, info) in enumerate(motors_info.items()):
+            self.groups[name] = QtWidgets.QGroupBox(name)
+            self.groups[name].setAlignment(Qt.AlignmentFlag.AlignHCenter)
             for ax in info.axes:
-                label = QtWidgets.QLabel(f"<strong>{ax}</strong>")
-                label.setTextFormat(Qt.TextFormat.RichText)
-                labels["label:" + ax] = label
-
-                label = QtWidgets.QLabel(f"<strong>{0:.2f} µm</strong>")
-                label.setTextFormat(Qt.TextFormat.RichText)
-                labels["pos:" + ax] = label
-
-                button = QtWidgets.QPushButton("+")
-                buttons["up:" + ax] = button
-
-                button = QtWidgets.QPushButton("-")
-                buttons["down:" + ax] = button
-
-                lineEdit = QtWidgets.QLineEdit(str(info.step_size))
-                lineEdits["step:" + ax] = lineEdit
-
-                label = QtWidgets.QLabel(str(" {}".format(info.step_egu)))
-                labels["step_egu:" + ax] = QtWidgets.QLabel(" µm")
+                self.labels["label:{}:{}".format(name, ax)] = QtWidgets.QLabel(
+                    f"<strong>{ax}</strong>"
+                )
+                self.labels["label:{}:{}".format(name, ax)].setTextFormat(
+                    Qt.TextFormat.RichText
+                )
+                self.labels["pos:{}:{}".format(name, ax)] = QtWidgets.QLabel(
+                    f"<strong>{0:.2f} µm</strong>"
+                )
+                self.labels["pos:{}:{}".format(name, ax)].setTextFormat(
+                    Qt.TextFormat.RichText
+                )
+                self.buttons["up:{}:{}".format(name, ax)] = QtWidgets.QPushButton("+")
+                self.buttons["down:{}:{}".format(name, ax)] = QtWidgets.QPushButton("-")
+                self.lineEdits["step:{}:{}".format(name, ax)] = QtWidgets.QLineEdit(
+                    str(info.step_size)
+                )
+                self.labels["step_egu:{}:{}".format(name, ax)] = QtWidgets.QLabel(
+                    str(" {}".format(info.step_egu))
+                )
 
                 # signal connection
-                buttons["up:" + ax].clicked.connect(
+                self.buttons["up:{}:{}".format(name, ax)].clicked.connect(
                     lambda *_, axis=ax: self.sigStepUp.emit(name, axis)
                 )
-                buttons["down:" + ax].clicked.connect(
+                self.buttons["down:{}:{}".format(name, ax)].clicked.connect(
                     lambda *_, axis=ax: self.sigStepDown.emit(name, axis)
                 )
+                self.lineEdits["step:{}:{}".format(name, ax)].textEdited.connect(
+                    lambda *_, name=name, axis=ax: self._validate_and_notify(name, axis)
+                )
+
+                self.grid.addWidget(
+                    self.labels["label:{}:{}".format(name, ax)], num_motor, 0
+                )
+                self.grid.addWidget(
+                    self.labels["pos:{}:{}".format(name, ax)], num_motor, 1
+                )
+                self.grid.addWidget(
+                    self.buttons["up:{}:{}".format(name, ax)], num_motor, 2
+                )
+                self.grid.addWidget(
+                    self.buttons["down:{}:{}".format(name, ax)], num_motor, 3
+                )
+                self.grid.addWidget(QtWidgets.QLabel("Step"), num_motor, 4)
+                self.grid.addWidget(
+                    self.lineEdits["step:{}:{}".format(name, ax)], num_motor, 5
+                )
+                self.grid.addWidget(
+                    self.labels["step_egu:{}:{}".format(name, ax)], num_motor, 6
+                )
+
+    def _validate_and_notify(self, name: str, axis: str) -> None:
+        """Validate the new step size value and notify the virtual bus when input is accepted.
+
+        Parameters
+        ----------
+        name : str
+            Motor name.
+        axis : str
+            Motor axis.
+        """
+        # Validate the input using the validator
+        input = self.lineEdits["step:{}:{}".format(name, axis)].text()
+
+        # can't do much for type hinting here since the validator returns
+        # a non-annotated tuple of unknown types
+        value: Tuple[QRegularExpressionValidator.State, Any, Any] = (
+            self.validator.validate(input, 0)
+        )  # type: ignore[assignment]
+        state = value[0]
+
+        if state == QRegularExpressionValidator.State.Invalid:
+            # Set red border if input is invalid
+            self.lineEdits["step:{}:{}".format(name, axis)].setStyleSheet(
+                "border: 2px solid red;"
+            )
+        else:
+            # Set default border if input is valid
+            self.lineEdits["step:{}:{}".format(name, axis)].setStyleSheet("")
+
+        # only emit when input is acceptable, discarding intermediate state
+        if state == QRegularExpressionValidator.State.Acceptable:
+            self.debug(f"Step size changed: {name}, {axis}, {input}")
+            self.sigStepSizeChanged.emit(name, axis, float(input))
