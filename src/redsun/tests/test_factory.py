4fee85a484fa3f93195d2f22b516e3f8d17ee309
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 15:17:16 +0100

    Remove factory tests
    - requires rework

--- src/redsun/tests/test_factory.py
+++
@@ -1,9 +0,0 @@
-from redsun.controller.factory import get_available_engines
-from redsun.engine.exengine import ExEngineHandler
-
-def test_handlers_dict() -> None:
-    """Test the content of the handlers dictionary."""
-    engines = get_available_engines()
-    assert len(engines) > 0
-    assert "exengine" in engines
-    assert engines["exengine"] == ExEngineHandler
