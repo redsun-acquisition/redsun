ae5b4ffde3407e97d56b3d8045cf4dbe75bf85e9
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 15:01:33 +0100

    Comment factory module
    - requires rethinking

--- src/redsun/controller/factory.py
+++ src/redsun/controller/factory.py
@@ -8,107 +8,107 @@ to create a unique running instance.
 This module operates within the RedSun core code and is not exposed to the toolkit or the user.
 """
 
-import importlib
-import inspect
-import os
-from typing import TYPE_CHECKING
+# import importlib
+# import inspect
+# import os
+# from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from typing import Type
+# if TYPE_CHECKING:
+#     from typing import Type
 
-    from sunflare.config import RedSunInstanceInfo, AcquisitionEngineTypes
-    from sunflare.engine import EngineHandler
-    from sunflare.virtualbus import VirtualBus
+#     from sunflare.config import RedSunInstanceInfo, AcquisitionEngineTypes
+#     from sunflare.engine import EngineHandler
+#     from sunflare.virtualbus import VirtualBus
 
-__all__ = ["get_available_engines", "create_engine", "ControllerFactory"]
+# __all__ = ["get_available_engines", "create_engine", "ControllerFactory"]
 
-# Initialize an empty dictionary for the handlers
-_HANDLERS: "dict[str, Type[EngineHandler]]" = {}
-
-
-def get_available_engines() -> "dict[str, Type[EngineHandler]]":
-    """Get a dictionary of available engine handlers.
-
-    Returns
-    -------
-    dict[str, Type[EngineHandler]]
-        Dictionary of available engine handlers.
-    """
-    global _HANDLERS
-
-    # base path for the engines directory
-    engines_path = os.path.join(os.path.dirname(__file__), "..", "engine")
-
-    if len(_HANDLERS) > 0:
-        return _HANDLERS
-
-    # Dynamically load all engine handlers
-    for engine in os.listdir(engines_path):
-        for file in os.listdir(os.path.join(engines_path, engine)):
-            # Engine-specific handlers are stored in handler.py;
-            # each engine has its own handler.py file
-            if file == "handler.py":
-                module_name = f"redsun.engine.{engine}"
-                module = importlib.import_module(
-                    module_name, file[-3]
-                )  # Import the module
-
-                for _, obj in inspect.getmembers(module, inspect.isclass):
-                    # Check if the class is a subclass of EngineHandler (to ensure it's a valid handler)
-                    if "EngineHandler" in [base.__name__ for base in obj.__bases__]:
-                        # Add the class to the handlers dictionary
-                        _HANDLERS[engine] = obj
-    return _HANDLERS
+# # Initialize an empty dictionary for the handlers
+# _HANDLERS: "dict[str, Type[EngineHandler]]" = {}
 
 
-def create_engine(
-    info: "RedSunInstanceInfo", virtual_bus: "VirtualBus", module_bus: "VirtualBus"
-) -> "EngineHandler":
-    """Create the proper engine handler based on the instance configuration.
+# def get_available_engines() -> "dict[str, Type[EngineHandler]]":
+#     """Get a dictionary of available engine handlers.
 
-    Parameters
-    ----------
-    info : RedSunInstanceInfo
-        RedSun instance configuration dataclass.
-    virtual_bus : VirtualBus
-        Intra-module virtual bus.
-    module_bus : VirtualBus
-        Inter-module virtual bus.
+#     Returns
+#     -------
+#     dict[str, Type[EngineHandler]]
+#         Dictionary of available engine handlers.
+#     """
+#     global _HANDLERS
 
-    Returns
-    -------
-    EngineHandler
-        Engine handler instance. The `EngineHandler` abstract class provides the API interface for all engine handlers.
+#     # base path for the engines directory
+#     engines_path = os.path.join(os.path.dirname(__file__), "..", "engine")
 
-    Raises
-    ------
-    ValueError
-        If the engine type is not recognized.
-    """
-    if len(_HANDLERS) == 0:
-        get_available_engines()
+#     if len(_HANDLERS) > 0:
+#         return _HANDLERS
 
-    try:
-        handler = _HANDLERS[info.engine]
-    except KeyError:
-        raise ValueError(f"Unknown engine: {info.engine}")
-    return handler(info, virtual_bus, module_bus)
+#     # Dynamically load all engine handlers
+#     for engine in os.listdir(engines_path):
+#         for file in os.listdir(os.path.join(engines_path, engine)):
+#             # Engine-specific handlers are stored in handler.py;
+#             # each engine has its own handler.py file
+#             if file == "handler.py":
+#                 module_name = f"redsun.engine.{engine}"
+#                 module = importlib.import_module(
+#                     module_name, file[-3]
+#                 )  # Import the module
+
+#                 for _, obj in inspect.getmembers(module, inspect.isclass):
+#                     # Check if the class is a subclass of EngineHandler (to ensure it's a valid handler)
+#                     if "EngineHandler" in [base.__name__ for base in obj.__bases__]:
+#                         # Add the class to the handlers dictionary
+#                         _HANDLERS[engine] = obj
+#     return _HANDLERS
 
 
-def get_engine_handler(engine: "AcquisitionEngineTypes") -> "Type[EngineHandler]":
-    """Get the engine handler class for a given engine.
+# def create_engine(
+#     info: "RedSunInstanceInfo", virtual_bus: "VirtualBus", module_bus: "VirtualBus"
+# ) -> "EngineHandler":
+#     """Create the proper engine handler based on the instance configuration.
 
-    Parameters
-    ----------
-    engine : str
-        Engine name.
+#     Parameters
+#     ----------
+#     info : RedSunInstanceInfo
+#         RedSun instance configuration dataclass.
+#     virtual_bus : VirtualBus
+#         Intra-module virtual bus.
+#     module_bus : VirtualBus
+#         Inter-module virtual bus.
 
-    Returns
-    -------
-    Type[EngineHandler]
-        Engine handler class.
-    """
-    if len(_HANDLERS) == 0:
-        get_available_engines()
+#     Returns
+#     -------
+#     EngineHandler
+#         Engine handler instance. The `EngineHandler` abstract class provides the API interface for all engine handlers.
 
-    return _HANDLERS[engine].instance()
+#     Raises
+#     ------
+#     ValueError
+#         If the engine type is not recognized.
+#     """
+#     if len(_HANDLERS) == 0:
+#         get_available_engines()
+
+#     try:
+#         handler = _HANDLERS[info.engine]
+#     except KeyError:
+#         raise ValueError(f"Unknown engine: {info.engine}")
+#     return handler(info, virtual_bus, module_bus)
+
+
+# def get_engine_handler(engine: "AcquisitionEngineTypes") -> "Type[EngineHandler]":
+#     """Get the engine handler class for a given engine.
+
+#     Parameters
+#     ----------
+#     engine : str
+#         Engine name.
+
+#     Returns
+#     -------
+#     Type[EngineHandler]
+#         Engine handler class.
+#     """
+#     if len(_HANDLERS) == 0:
+#         get_available_engines()
+
+#     return _HANDLERS[engine].instance()
