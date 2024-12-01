799fe03ff89759c6deba48bdfcaec83346fa8df1
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-01 18:40:53 +0100

    Use __future__ and add internal signals

--- src/redsun/controller/virtualbus.py
+++ src/redsun/controller/virtualbus.py
@@ -1,11 +1,14 @@
 """RedSun core virtual bus implementation."""
 
-from typing import final
+from __future__ import annotations
+
+from typing import final, TYPE_CHECKING
+
 from sunflare.virtualbus import VirtualBus
-from typing import TYPE_CHECKING
+from sunflare.virtualbus import Signal
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from typing import Optional, ClassVar
 
 __all__ = ["HardwareVirtualBus"]
 
@@ -19,10 +22,16 @@ class HardwareVirtualBus(VirtualBus):
     See the `VirtualBus` class for API information.
     """
 
-    __instance: "Optional[HardwareVirtualBus]" = None
+    # TODO: these need to be documented with
+    # the description field in the Signal class;
+    # but first the virtual bus needs to be reworked
+    sigStepperStepUp: Signal = Signal(str, str)
+    sigStepperStepDown: Signal = Signal(str, str)
+
+    __instance: ClassVar[Optional[HardwareVirtualBus]] = None
 
     @classmethod
-    def instance(cls) -> "HardwareVirtualBus":
+    def instance(cls) -> HardwareVirtualBus:
         """Return global HardwareVirtualBus singleton instance."""
         if cls.__instance is None:
             cls.__instance = cls()
