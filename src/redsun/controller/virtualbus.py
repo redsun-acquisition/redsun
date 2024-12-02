dbabe0a535cf56cae89bf045a322476bdcd73b04
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-02 23:53:38 +0100

    Update

--- src/redsun/controller/virtualbus.py
+++ src/redsun/controller/virtualbus.py
@@ -2,15 +2,19 @@
 
 from __future__ import annotations
 
-from typing import final, TYPE_CHECKING
+from typing import final, TYPE_CHECKING, TypeAlias
 
 from sunflare.virtualbus import VirtualBus
 from sunflare.virtualbus import Signal
+from sunflare.types import AxisLocation
+from sunflare.config import MotorModelTypes
 
 if TYPE_CHECKING:
-    from typing import Optional, ClassVar
+    from typing import Optional, ClassVar, Union
 
 __all__ = ["HardwareVirtualBus"]
+
+TA: TypeAlias = Union[int, float, str]
 
 
 @final
@@ -22,8 +26,60 @@ class HardwareVirtualBus(VirtualBus):
     See the `VirtualBus` class for API information.
     """
 
-    sigStepperStepUp: Signal = Signal(str, str)
-    sigStepperStepDown: Signal = Signal(str, str)
+    sigStepperStepUp: Signal = Signal(
+        str,
+        str,
+        description="""
+Emitted when the user clicks the up button for a stepper motor axis.
+
+Source: StepperMotorWidget
+
+Parameters
+----------
+motor : str
+    Motor name.
+axis : str
+    Motor axis.
+""",
+    )
+    sigStepperStepDown: Signal = Signal(
+        str,
+        str,
+        description="""
+Emitted when the user clicks the down button for a stepper motor axis.
+
+Source: StepperMotorWidget
+
+Parameters
+----------
+motor : str
+    Motor name.
+axis : str
+    Motor axis.
+""",
+    )
+    sigStepSizeChanged: Signal = Signal(
+        str,
+        str,
+        float,
+        description="""
+Emitted when the user changes the step size for a stepper motor axis.
+The new value is transmitted after being validated by the widget.
+If the input is invalid, the signal is not emitted.
+
+Source: StepperMotorWidget
+
+Parameters
+----------
+motor : str
+    Motor name.
+axis : str
+    Motor axis.
+step_size : float
+    New step size.
+""",
+    )
+    sigMoveDone: Signal = Signal(str, MotorModelTypes, AxisLocation[TA])
 
     __instance: ClassVar[Optional[HardwareVirtualBus]] = None
 
