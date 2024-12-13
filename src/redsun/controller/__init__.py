5f46d21dc12117d14e1226e4ec237b6f94f0a541
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-13 10:18:43 +0100

    Export classes in __init__

--- src/redsun/controller/__init__.py
+++ src/redsun/controller/__init__.py
@@ -1,1 +1,7 @@
 # noqa: D104
+
+from .hardware import RedsunMainHardwareController
+from .virtualbus import HardwareVirtualBus
+from .plugins import PluginManager
+
+__all__ = ["RedsunMainHardwareController", "HardwareVirtualBus", "PluginManager"]
