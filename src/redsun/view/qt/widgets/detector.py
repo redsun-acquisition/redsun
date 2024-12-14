d8ab8ddb3d67f7cbfb5121a2ca20bbc801b3a68e
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-14 23:11:26 +0100

    Add missing abstract methods

--- src/redsun/view/qt/widgets/detector.py
+++ src/redsun/view/qt/widgets/detector.py
@@ -54,36 +54,84 @@ class DetectorSettingsWidget(BaseWidget):
         for detector_name, detector_info in detectors_info.items():
             tree = self.build_params(detector_info)
             self.tab.addTab(tree, detector_name)
-        
+
         layout.addWidget(self.tab)
         self.setLayout(layout)
+
+    def registration_phase(self) -> None:  # noqa: D102
+        # nothing to do here
+        ...
+
+    def connection_phase(self) -> None:  # noqa: D102
+        # nothing to do here... for now
+        ...
 
     # TODO: parameter grouping needs review
     # TODO: generalize for all parameter groups
     def build_params(self, detector_info: DetectorModelInfo) -> ParameterTree:
         """Build a parameter tree for the detector settings.
-        
+
         Parameters
         ----------
         detector_info : DetectorModelInfo
             The detector model information.
-        
+
         Returns
         -------
         ParameterTree
             The parameter tree for the detector settings.
         """
         params = [
-            {'name': 'Model Name', 'type': 'str', 'value': detector_info.model_name, 'readonly': True},
-            {'name': 'Supported Engines', 'type': 'list', 'value': detector_info.supported_engines[0], 'readonly': True},
-            {'name': 'Vendor', 'type': 'str', 'value': detector_info.vendor, 'readonly': True},
-            {'name': 'Serial Number', 'type': 'str', 'value': detector_info.serial_number, 'readonly': True},
-            {'name': 'Category', 'type': 'str', 'value': detector_info.category, 'readonly': True},
-            {'name': 'Sensor Size', 'type': 'str', 'value': str(detector_info.sensor_size), 'readonly': True},
-            {'name': 'Pixel Size', 'type': 'str', 'value': str(detector_info.pixel_size)},
-            {'name': 'Exposure EGU', 'type': 'str', 'value': detector_info.exposure_egu},
+            {
+                "name": "Model Name",
+                "type": "str",
+                "value": detector_info.model_name,
+                "readonly": True,
+            },
+            {
+                "name": "Supported Engines",
+                "type": "list",
+                "value": detector_info.supported_engines[0],
+                "readonly": True,
+            },
+            {
+                "name": "Vendor",
+                "type": "str",
+                "value": detector_info.vendor,
+                "readonly": True,
+            },
+            {
+                "name": "Serial Number",
+                "type": "str",
+                "value": detector_info.serial_number,
+                "readonly": True,
+            },
+            {
+                "name": "Category",
+                "type": "str",
+                "value": detector_info.category,
+                "readonly": True,
+            },
+            {
+                "name": "Sensor Size",
+                "type": "str",
+                "value": str(detector_info.sensor_size),
+                "readonly": True,
+            },
+            {
+                "name": "Pixel Size",
+                "type": "str",
+                "value": str(detector_info.pixel_size),
+            },
+            {
+                "name": "Exposure EGU",
+                "type": "str",
+                "value": detector_info.exposure_egu,
+            },
         ]
-        parameter = Parameter.create(name='Detector parameters', type='group', children=params)
+        parameter = Parameter.create(
+            name="Detector parameters", type="group", children=params
+        )
         tree = ParameterTree()
         tree.setParameters(parameter, showTop=True)
         return tree
