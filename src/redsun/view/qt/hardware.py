ab3bcf8c541375c46b591bb1f2ecbcc45133f836
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-14 16:40:44 +0100

    Make it as a widget container
    - widgets are then allocated in the main window in mainview.py

--- src/redsun/view/qt/hardware.py
+++ src/redsun/view/qt/hardware.py
@@ -1,13 +1,45 @@
 # noqa: D100
 from __future__ import annotations
 
-from qtpy.QtWidgets import QMainWindow
+from typing import TYPE_CHECKING
 
-from sunflare.log import Loggable
+if TYPE_CHECKING:
+    from redsun.view.qt.widgets import ImageViewWidget
+    from sunflare.view.qt import BaseWidget
 
 
-class RedSunMainHardwareWidget(QMainWindow, Loggable):  # noqa: D101
-    """RedSun main hardware widget."""
+# TODO: maybe do a dataclass...?
+class RedSunHardwareWidgetContainer:  # noqa: D101
+    """RedSun hardware widget container class.
 
-    def __init__(self, title: str) -> None:
-        super().__init__()
+    This is not a real widget class, but only a container for the widget references.
+    These are then instantiated in the main view window according to a specific layout.
+    """
+
+    # image widget: center of the main window
+    _image_viewer: ImageViewWidget
+
+    # device widgets: left side of the main window
+    _device_widgets: dict[str, BaseWidget] = {}
+
+    # controller widgets: right side of the main window
+    _controller_widgets: dict[str, BaseWidget] = {}
+
+    @property
+    def image_viewer(self) -> ImageViewWidget:
+        """Image viewer widget."""
+        return self._image_viewer
+
+    @image_viewer.setter
+    def image_viewer(self, widget: ImageViewWidget) -> None:
+        self._image_viewer = widget
+
+    @property
+    def device_widgets(self) -> dict[str, BaseWidget]:
+        """Device widgets."""
+        return self._device_widgets
+
+    @property
+    def controller_widgets(self) -> dict[str, BaseWidget]:
+        """Controller widgets."""
+        return self._controller_widgets
