3e410c25f5c1bd53e2ce67e14be24169ca3645d7
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-18 10:09:11 +0100

    Ignore missing version import

--- src/redsun/__init__.py
+++ src/redsun/__init__.py
@@ -1,5 +1,5 @@
 # noqa : D100
 try:
-    from ._version import version as __version__
+    from ._version import version as __version__  # type: ignore[import-not-found]
 except ImportError:
     __version__ = "unknown"
