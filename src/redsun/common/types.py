f0e6a3da3873b3a1ec03811f1528bbab72e4eb33
Jacopo Abramo <jacopo.abramo@gmail.com>
2024-12-24 16:36:25 +0100

    Remove typed dict
    - relevant code moved to sunflare

--- src/redsun/common/types.py
+++ src/redsun/common/types.py
@@ -2,59 +2,21 @@
 
 from __future__ import annotations
 
-from typing import Any, Optional, Tuple, Type, TypeAlias, TypedDict, Union
+from typing import Tuple, Type, TypeAlias, Union
 
 from sunflare.config import (
-    AcquisitionEngineTypes,
     ControllerInfo,
     DetectorModelInfo,
-    FrontendTypes,
     MotorModelInfo,
 )
 from sunflare.controller import BaseController
 from sunflare.engine import DetectorModel, MotorModel
 
-InfoTypes: TypeAlias = Union[
-    Type[DetectorModelInfo], Type[MotorModelInfo], Type[ControllerInfo]
-]
-BuildTypes: TypeAlias = Union[
+InfoTypes = Union[Type[DetectorModelInfo], Type[MotorModelInfo], Type[ControllerInfo]]
+BuildTypes = Union[
     Type[DetectorModel[DetectorModelInfo]],
     Type[MotorModel[MotorModelInfo]],
     Type[BaseController],
 ]
 
-Registry: TypeAlias = dict[str, list[Tuple[str, InfoTypes, BuildTypes]]]
-
-
-# TODO: RedSunInstanceInfo should be replaced by this. The original intent was to be able to
-#       dump to file via pydantic the configuration information. This may be a better
-#       approach and more coherent with the plugin-driven architecture.
-# TODO: which types should be used for the dictionary fields?
-# TODO: maybe this approach makes sense
-#       https://techoverflow.net/2024/09/25/pydantic-how-to-load-store-model-in-yaml-file-minimal-example/
-class RedSunConfigInfo(TypedDict):
-    """Typed dictionary for RedSun configuration.
-
-    The dictionary holds the information parsed from the input configuration file.
-    They are used from the internal factory classes to build the model and controller
-    information objects. The latter are later built into the main hardware controller.
-
-    Parameters
-    ----------
-    engine : ``sunflare.config.AcquisitionEngineTypes``
-        Selected acquisition engine.
-    frontend : ``sunflare.config.FrontendTypes``
-        Selected frontend.
-    motors : ``dict[str, sunflare.config.MotorModelInfo]``
-        Dictionary containing motor models information.
-    detectors : ``dict[str, sunflare.config.DetectorModelInfo]``
-        Dictionary containing detector models information.
-    controllers : ``dict[str, sunflare.config.ControllerInfo]``
-        Dictionary containing controller information.
-    """
-
-    engine: AcquisitionEngineTypes
-    frontend: FrontendTypes
-    motors: Optional[dict[str, dict[str, Any]]]
-    detectors: Optional[dict[str, dict[str, Any]]]
-    controllers: Optional[dict[str, dict[str, Any]]]
+Registry = dict[str, list[Tuple[str, InfoTypes, BuildTypes]]]
