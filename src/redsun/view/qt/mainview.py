d95a1c3385e9dafff2eb0183e044a47afd19cd82
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-02-04 19:31:35 +0100

    Adding more implementation in mainview

--- src/redsun/view/qt/mainview.py
+++ src/redsun/view/qt/mainview.py
@@ -2,10 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QMainWindow
+from qtpy import QtWidgets
+
+from sunflare.log import Loggable
+from sunflare.config import WidgetPositionTypes
 
 if TYPE_CHECKING:
     from sunflare.config import RedSunSessionInfo
@@ -13,14 +16,25 @@ if TYPE_CHECKING:
     from sunflare.virtual import VirtualBus
 
 
-class RedSunMainWindow(QMainWindow):
+class RedSunMainWindow(QtWidgets.QMainWindow, Loggable):
     """RedSun main window.
 
     Parameters
     ----------
-    controller : RedsunMainHardwareController
-        RedSun main hardware controller. Contains all the references to the backend objects.
+    virtual_bus : :class:`~sunflare.virtual.VirtualBus`
+        Session virtual bus.
+    config : :class:`~sunflare.config.RedSunSessionInfo`
+        Session configuration.
+    widgets : dict[str, type[:class:`~sunflare.view.qt.BaseQtWidget`]]
+        Dictionary with the widget names and their types.
     """
+
+    _DOCK_MAP = {
+        WidgetPositionTypes.LEFT: Qt.DockWidgetArea.LeftDockWidgetArea,
+        WidgetPositionTypes.RIGHT: Qt.DockWidgetArea.RightDockWidgetArea,
+        WidgetPositionTypes.TOP: Qt.DockWidgetArea.TopDockWidgetArea,
+        WidgetPositionTypes.BOTTOM: Qt.DockWidgetArea.BottomDockWidgetArea,
+    }
 
     def __init__(
         self,
@@ -33,15 +47,59 @@ class RedSunMainWindow(QMainWindow):
         self._config = config
         self._virtual_bus = virtual_bus
 
-        # custom widgets (need to be built)
-        # TODO: build them; API
-        self._widgets = widgets
+        self._widgets: dict[str, BaseQtWidget] = {}
+        self.build_view(widgets)
 
-    def build_view(self) -> None:
-        """Build the main view window."""
-        # TODO: build the widgets here
+        self._menu_bar: QtWidgets.QMenuBar
+
+        # let's make mypy happy...
+        self._menu_bar = cast(QtWidgets.QMenuBar, self.menuBar())
+        # file = self._menu_bar.addMenu("&File")
+
+        # "File" actions
+        # TODO: something is wrong with mypy/pyright;
+        # need to adjust the type hinting before proceeding
+        # ... or alternatively just ignore the error
+        # self._save_action = QtWidgets.QAction("Save configuration as...", self)
+
+    def build_view(self, widget_types: dict[str, type[BaseQtWidget]]) -> None:
+        """Build the main view window.
+
+        Iterates over `widget_types` and creates the widgets.
+        The widgets are docked according to the associated value of
+        ``config.widgets[widget_name].position``. If said value is
+        :class:`~sunflare.config.WidgetPositionTypes.CENTER`,
+        the widget is set as the central widget.
+
+        Parameters
+        ----------
+        widget_types : ``dict[str, type[BaseQtWidget]]``
+            Dictionary with the widget names and their types.
+        """
+        for name, widget_type in widget_types.items():
+            widget = widget_type(self._config, self._virtual_bus)
+            self._widgets[name] = widget
+            try:
+                cfg = self._config.widgets[name]
+                dock_area = self._DOCK_MAP[cfg.position]
+                dock_widget = QtWidgets.QDockWidget(name, widget)
+                self.addDockWidget(dock_area, dock_widget)
+            except KeyError:
+                # assuming that current widget
+                # should be set as central widget
+                if self.centralWidget() is None:
+                    self.setCentralWidget(widget)
+                else:
+                    self.error(f"Multiple central widgets are not allowed: {name}")
+            widget.registration_phase()
+            self._widgets[name] = widget
 
         self.setWindowState(Qt.WindowState.WindowMaximized)
 
     def connect_to_virtual(self) -> None:
-        """Connect the view to the virtual layer."""
+        """Connect the view to the virtual layer.
+
+        Iterates over all widgets and calls their `connection_phase` method.
+        """
+        for widget in self._widgets.values():
+            widget.connection_phase()
