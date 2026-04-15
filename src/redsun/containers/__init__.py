e6fc351755e65a23b6368d402dc56bb9cf82d0bb
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-04-15 16:32:26 +0200

    refactor!: rename component declaration functions

--- src/redsun/containers/__init__.py
+++ src/redsun/containers/__init__.py
@@ -3,14 +3,14 @@
 from __future__ import annotations
 
 from ._config import AppConfig
-from .components import device, presenter, view
+from .components import declare_device, declare_presenter, declare_view
 from .container import AppContainer, Frontend
 
 __all__ = [
     "AppConfig",
     "AppContainer",
     "Frontend",
-    "device",
-    "presenter",
-    "view",
+    "declare_device",
+    "declare_presenter",
+    "declare_view",
 ]
