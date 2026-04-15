c2d5ba05c2c9a7db173779370bd8e05ee4e0f7ad
Jacopo Abramo <jacopo.abramo@gmail.com>
2026-04-15 17:13:26 +0200

    refactor!: export new function names

--- src/redsun/__init__.py
+++ src/redsun/__init__.py
@@ -1,10 +1,23 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from redsun.containers import AppContainer, Frontend, device, presenter, view
+from redsun.containers import (
+    AppContainer,
+    Frontend,
+    declare_device,
+    declare_presenter,
+    declare_view,
+)
 
 try:
     __version__ = version("redsun")
 except PackageNotFoundError:
     __version__ = "unknown"
 
-__all__ = ["AppContainer", "Frontend", "view", "presenter", "device", "__version__"]
+__all__ = [
+    "AppContainer",
+    "Frontend",
+    "declare_view",
+    "declare_presenter",
+    "declare_device",
+    "__version__",
+]
